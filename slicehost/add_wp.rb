c24b2c9fa81cd6c0ef6232e791926837d1db5721
Marc Chung <mchung@gmail.com>
2010-05-09 16:19:00 -0700

    Updating wp

--- slicehost/add_wp.rb
+++ slicehost/add_wp.rb
@@ -47,9 +47,12 @@ dbname = WORDPRESS[:dbname]
 dbuser = WORDPRESS[:dbuser]
 dbpass = WORDPRESS[:dbpass]
 
+raise "Requires --wordpress, --dbname, --dbuser, --dbpass" unless wpdom && dbname && dbuser && dbpass
+
 puts "mkdir -p /var/local/wp/#{wpdom}/{public,private,log,backup}"
 puts "copy over #{wpdom} the nginx wordpress # need to generate it and dump it into /etc/nginx/sites-available/"
 puts "ln -s /etc/nginx/sites-available/#{wpdom} /etc/nginx/sites-enabled/#{wpdom}"
+puts ""
 
 puts "make-ssl-cert /usr/share/ssl-cert/ssleay.cnf /etc/ssl/certs/selfsigned.pem # Use ^h to backspace"
 puts "cd /var/local/wp/#{wpdom}/private"
@@ -61,6 +64,7 @@ puts "openssl x509 -req -days 3650 -in #{wpdom}.csr -signkey #{wpdom}.key -out #
 puts "cp #{wpdom}.crt /etc/ssl/certs/"
 puts "cp #{wpdom}.key /etc/ssl/private/"
 puts "service nginx restart"
+puts ""
 
 cat =<<EOF
 CREATE DATABASE #{dbname};
@@ -69,15 +73,17 @@ EOF
 # cat > /tmp/mysql.sql <<EOF
 puts cat
 puts "mysql -u root -p < /tmp/mysql.sql"
+puts ""
 
 puts "cd /var/local/#{wpdom}/public"
 puts "svn co http://svn.automattic.com/wordpress/tags/2.9.2 ."
 puts "chown -R app:app /var/local/#{wpdom}"
 puts "chmod -R 755 /var/local#{wpdom}"
 puts "chmod -R -s /var/local#{wpdom}"
+puts ""
 
 puts "Setup wordpress"
-
 puts "Add to wp-config.php"
 puts "define('FORCE_SSL_LOGIN', true);"
 puts "define('FORCE_SSL_ADMIN', true);"
+puts ""
