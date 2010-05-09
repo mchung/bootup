69bd21bee70e4218de4c679e1b9e1d7385767f26
Marc Chung <mchung@gmail.com>
2010-05-09 11:50:13 -0700

    updating php fastcgi

--- slicehost/60-php.sh
+++ slicehost/60-php.sh
@@ -8,12 +8,12 @@ aptitude update -y
 
 aptitude install -y php5-common php5-cgi php5-mysql php5-cli php5-gd
 
-cp ~/bootstrap/config/initd-php5-fastcgi /etc/init.d/php-fastcgi
+cp ~/bootstrap/config/etc/init.d/php-fastcgi /etc/init.d/php-fastcgi
 chmod 755 /etc/init.d/php-fastcgi
 
-cp ~/bootstrap/config/default-php5-fastcgi /etc/default/php-fastcgi
+cp ~/bootstrap/config/etc/default/php-fastcgi /etc/default/php-fastcgi
 
-cp ~/bootstrap/config/fastcgi_params /etc/nginx/fastcgi_params
+cp ~/bootstrap/config/etc/nginx/fastcgi_params /etc/nginx/fastcgi_params
 
 /etc/init.d/php-fastcgi start
 update-rc.d php-fastcgi defaults
